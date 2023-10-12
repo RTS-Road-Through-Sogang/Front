f5a0afd5604899143346a805247569e1219195f9
Sehwan-Chang <kdwkdwkd@naver.com>
2023-10-12 20:00:36 +0900

    first setting

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        textAlign: "center",
+        borderBottom: "2px solid #D9D9D9",
+        lineHeight: "50px",
+        marginTop: "50px",
+      }}
+    >
+      <span style={{ background: "#fff", padding: "0 0px" }}></span>
+    </div>
+  );
+};
+
+export default Header;
