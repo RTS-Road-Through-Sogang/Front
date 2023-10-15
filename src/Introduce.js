73045dc67d4d402a68a7698ae2fe57e134d4d356
SehwanChang <kdwkdwkd@naver.com>
2023-10-15 23:56:12 +0900

    style changed

--- src/Introduce.js
+++ src/Introduce.js
@@ -4,44 +4,56 @@ import intro_image from "./images/main.jpeg";
 
 const Introduce = () => {
   return (
-    <>
-      <Container>
+    <Container>
+      <ImageWrapper>
         <Image src={intro_image} alt="Intro" />
-        <Text>
-          <DiffText> RTS</DiffText>란 무엇인가요
-          <DiffText>?</DiffText>
-        </Text>
-      </Container>
-    </>
+      </ImageWrapper>
+      <Text>
+        복수전공, 부전공... 학교 생활은 항상 선택의 연속입니다.
+        <br />
+        <DiffText>RTS </DiffText>는 그 선택을 조금 더 간편하게 도와주는{" "}
+        <DiffText>로드맵 플랫폼 </DiffText>
+        입니다.
+      </Text>
+    </Container>
   );
 };
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
-  position: relative;
-  width: 32%;
+  align-items: center;
+  justify-content: space-between; // 여기에 추가
+  width: 70%;
   margin: 50px auto;
+  border: 1px solid grey;
 
   @media (max-width: 768px) {
-    width: 40%;
-    padding: 40 20px; 
+    flex-direction: column;
+    width: 100%;
+    padding: 40px 20px;
+    justify-content: center;
   }
 `;
 
+const ImageWrapper = styled.div`
+  flex: 1;
+  margin-right: 10px;
+`;
+
 const Image = styled.img`
-  width: 120%;
+  width: 60%;
   object-fit: cover;
-  margin-left: -15%;
   opacity: 0.3;
 `;
 
 const Text = styled.div`
+  flex: 1;
   position: relative;
   color: black;
   font-weight: bold;
   padding: 10px;
   border-radius: 5px;
+  line-height: 5;
 `;
 
 const DiffText = styled.span`
