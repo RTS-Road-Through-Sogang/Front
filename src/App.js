4cba0f8e44e079b88bb634f789e3429f41ef8fcc
SojeongM <soj0227@naver.com>
2023-11-01 16:06:51 +0900

    sj_first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Header from "./Header";
 import Introduce from "./Introduce";
+import Selecttech from "./6_tech";
+import SelectSearch from "./6_search";
+import SelecMajor from "./6_major";
+import SelectSub from "./6_submajor";
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Introduce />
+      <SelectSub/>
     </div>
   );
 }
