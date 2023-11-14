16703979f2766e973636e005e8009ffabcac7a4c
SehwanChang <kdwkdwkd@naver.com>
2023-11-14 23:16:41 +0900

    route added

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import SelectCseGicho from "./Select_3_cse_gicho";
 import SelectCseDuty from "./Select_4_cseduty";
 import SelectCseDutyChoice from "./Select_5_csedutychoice";
 import SelectCseChoice from "./Select_6_cseselect";
+import Sehwan from "./Sehwan";
 
 export const URL = process.env.APIURL;
 
@@ -23,8 +24,11 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
+          <Route path="test" element={<Sehwan />} />
+          <Route path="/intro" element={<Introduce />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/roadmap" element={<Roadmap />} />
           <Route path="/majortrack" element={<SelecMajor />} />
           <Route path="/selectsearch" element={<SelectSearch />} />
           <Route path="/selectcommon" element={<SelectCommon />} />
